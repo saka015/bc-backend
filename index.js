d9e0243b4a514b55e5942933a95f00f6d583cf49
Kamal Nayan <sknasylum8454@gmail.com>
2024-02-18 01:44:56 +0530

    update

--- index.js
+++ index.js
@@ -2,33 +2,34 @@ import express from "express";
 import connect from "./db.js";
 import createUserRouter from "./Routes/CreateUser.js";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
-app.use(cors(
-  {
-    origin:["https://bizarre-coders-official.vercel.app/"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-))
-
+// app.use(
+//   cors({
+//     origin: ["https://bizarre-coders-official.vercel.app/"],
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
 
 const port = process.env.PORT;
 
 connect();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://bizarre-coders-official.vercel.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
-
 
 app.use(express.json());
 app.use("/api/", createUserRouter);
