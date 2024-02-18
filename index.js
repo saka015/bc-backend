2488c49fa55b3263fca367eff491706eaa4bb468
Kamal Nayan <sknasylum8454@gmail.com>
2024-02-18 12:09:55 +0530

    changes url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ connect();
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://bizarre-coders-official.vercel.app/"
+    "http://localhost:3000"
   );
   res.header(
     "Access-Control-Allow-Headers",
